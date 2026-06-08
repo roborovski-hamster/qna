dbc08f6d2e7c0289e363f75ac783a1f98c790560
roborovski-hamster <118609950+roborovski-hamster@users.noreply.github.com>
2026-06-08 14:38:41 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,36 @@
-const express = require('express');
+const express = require("express");
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({ result: 'success' });
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        status: "ok"
+    });
+});
+
+app.post("/skill", (req, res) => {
+
+    console.log(JSON.stringify(req.body, null, 2));
+
+    res.json({
+        version: "2.0",
+        template: {
+            outputs: [
+                {
+                    simpleText: {
+                        text: "카카오 스킬 호출 성공"
+                    }
+                }
+            ]
+        }
+    });
+
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
